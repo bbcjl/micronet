3d2ca13fe034214c536b5193a01dd71727150abc
James Livesey <jamesliveg@gmail.com>
2024-02-04 12:43:29 +0000

    Use `ping`/`pong` test values for now

--- src/main.js
+++ src/main.js
@@ -20,21 +20,14 @@ var manager = new conversations.ConversationManager(IS_SERVER ? 0x0BBC : 0xAAAA)
 manager.requestHandler = function(payload) {
     console.log("Received request:", payload.toString());
 
-    return Promise.resolve(Buffer.from("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+    return Promise.resolve(Buffer.from("pong"));
 };
 
 port.on("open", function() {
     console.log(`Modem connected; micro:net is up! (ID ${common.hex(manager.id)})`);
 
-    // setInterval(function() {
-    //     port.write("mm");
-    //     port.write(Buffer.from([0x01, 0x01, 4]));
-    //     port.write("test");
-    //     manager.update();
-    // }, 1000);
-
     if (!IS_SERVER) {
-        manager.createRequest(0x0BBC, Buffer.from("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nisl ut tellus tincidunt mollis. Mauris efficitur consequat pellentesque. Suspendisse dignissim est nunc. Curabitur venenatis eleifend tincidunt. Nunc laoreet rutrum nisi, ac efficitur lectus sodales non. Quisque id sapien hendrerit risus venenatis finibus. Mauris non arcu eu ipsum mollis ullamcorper. Nullam semper in lectus at luctus. Pellentesque luctus mi ut erat viverra vulputate. Suspendisse vestibulum urna orci, non varius libero efficitur eget. Cras maximus turpis mi, ut lobortis purus pharetra at. Vestibulum mattis eros eu lectus facilisis vehicula. Vestibulum cursus ac tellus eget porttitor. Duis id ullamcorper mi.")).then(function(payload) {
+        manager.createRequest(0x0BBC, Buffer.from("ping")).then(function(payload) {
             console.log("Received response:", payload.toString());
         });
     }
